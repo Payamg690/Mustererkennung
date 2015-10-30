cee8ef57edb720c32263698ac01eadb51440b21f
Jan Szynal <jan.szynal@gmail.com>
2015-10-30 16:11:11 +0100

    Aufg 1.3

--- run.m
+++ run.m
@@ -1,4 +1,7 @@
 [trainImages, trainLabels, testImages, testLabels] = readMNISTFiles;
  
 
-displayRandomDatapointsWithLabels(10, trainImages, trainLabels)
+%displayRandomDatapointsWithLabels(10, trainImages, trainLabels)
+
+%the second-last point's label:
+l = trainLabels(5999)
